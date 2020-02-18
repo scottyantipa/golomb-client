e860a85cbc09af31f06f48fb7d542e23e6c4def6
Scott Antipa <scottantipa@gmail.com>
2020-02-17 18:22:58 -0800

    Respond to and display basic live events: New search, Current values, End search...

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const SERVER_HOST = 'localhost';
 const SERVER_PORT = '8080';
 const SERVER_URL = `http://${SERVER_HOST}:${SERVER_PORT}`;
 
+enum SolverStates {
+  Searching = 'Searching',
+  Idle = 'Idle'
+}
+
 const App: React.SFC = () => {
+  const [solverState, setSolverState] = useState(SolverStates.Idle);
+  const [solution, setSolution] = useState(undefined);
+  const [intermediateSolution, setIntermediateSolution] = useState(undefined);
+  const [objBound, setObjBound] = useState<Number>(Infinity);
+  const [currentVars, setCurrentVars] = useState<String>("n/a");
+
   useEffect(
     () => {
       const socket = new WebSocket(`ws://${SERVER_HOST}:${SERVER_PORT}/ws`);
       socket.addEventListener('message', (evt) => {
-        console.log('message: ', evt.data);
+        const eventName = evt.data.split(':')[0];
+        const eventData = evt.data.split(':')[1];
+        switch (eventName) {
+          case 'StartSearch':
+            setSolverState(SolverStates.Searching);
+            setIntermediateSolution(undefined);
+            setSolution(undefined);
+            break;
+          case 'Periodic':
+            setSolverState(SolverStates.Searching);
+            setCurrentVars(eventData);
+            break;
+          case 'ObjBound':
+            setObjBound(parseInt(eventData));
+            break;
+          case 'NewSolution':
+            setIntermediateSolution(eventData);
+            break;
+          case 'EndSearch':
+            setSolverState(SolverStates.Idle);
+            break;
+          case 'Final':
+            setSolution(eventData);
+            break;
+          default:
+        }
       });
       return () => socket.close();
     },
     []
   );
+
   return (
     <div>
       <button onClick={() => {
@@ -22,6 +59,15 @@ const App: React.SFC = () => {
       }}>
         Solve
       </button>
+      <br />
+      <br />
+      <div style={{ whiteSpace: 'pre', margin: 20 }}>
+        <div>State:        {solverState}</div>
+        <div>Bound:        {objBound}</div>
+        <div>Current:      {currentVars}</div>
+        <div>Intermediate: {intermediateSolution || "n/a"}</div>
+        <div>Final:        {solution || "n/a"}</div>
+      </div>
     </div>
   )
 }
